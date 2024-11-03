e2349795a31b5580f5163e2aa5b8f1eeb4724a36
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2024-11-03 16:20:28 +0530

    Sidebar in react js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import "./App.scss"
+import React from "react";
+import "./App.scss";
 const App = () => {
   return (
-    <div>App
-      <button className='btn btn-success'>click</button>
+    <div>
+      App
+      <button className="btn btn-success">click</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
